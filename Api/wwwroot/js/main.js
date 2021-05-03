22fb1561f08d2b72bd06a83a4587f2db9b89355a
Jacob Brännström <jacob.z.brannstrom@teliacompany.com>
2021-05-03 17:11:47 +0200

    Missed committing javascript changes

--- Api/wwwroot/js/main.js
+++ Api/wwwroot/js/main.js
@@ -1,19 +1,18 @@
-var  PopulateData = new Vue({
+var PopulateData = new Vue({
     el: '#performance-checklist',
     data: function() {
         return {
             result: [],
-            form : [],
             errors: [],
             pagination: {
                 currentPage : 1,
                 totalPages: function () {
-                    return !this.hasData() ? 0 : Math.floor(result.length / this.itemsPerPage) + 1; 
+                    return !this.hasData() ? 0 : Math.floor(this.result.length / this.itemsPerPage) + 1; 
                 },
                 itemsPerPage: 5
             },
             loading: false,
-        }
+        };
     },
     created: function() {
         this.fetchJsonData()
@@ -23,7 +22,7 @@ var  PopulateData = new Vue({
                     index: i,
                     key: e,
                     value: null
-                }
+                };
             });
         });
     },
@@ -39,10 +38,10 @@ var  PopulateData = new Vue({
         },
     },
     methods: {
-        getPage() {
+        getPage(){ 
             var end = this.pagination.currentPage * this.pagination.itemsPerPage;
             var start = end - this.pagination.itemsPerPage;
-            var items = this.result.slice(start, end)
+            var items = this.result.slice(start, end);
             return  {
                 pagination: this.pagination,
                 items: items
@@ -55,23 +54,35 @@ var  PopulateData = new Vue({
             this.pagination.currentPage--;
         },
         fetchJsonData() {
-            loading = true;
+            this.loading = true;
             return fetch('/content/performance_checklist.json')
             .then(res => {
                 return res.json();
             })
             .then(json => {
-                loading=false;
+                this.loading=false;
                 return json;
             });
         },
         validateForm() {
-            var errors = [];
+            var validationErrors = [];
             this.result.forEach(item => {
                 if(!item.value) {
-                    errors.push(`Question ${item.index + 1} (page ${ Math.floor(item.index / this.pagination.itemsPerPage) + 1}) is not answered`)
+                    let newError = {
+                        question: item.index,
+                        page: Math.floor(item.index / this.pagination.itemsPerPage)
+                    };
+                    validationErrors.push(newError);
                 }
             });
+            var errors = [];
+            var totalPages = Math.floor(this.result.length / this.pagination.itemsPerPage);
+            for (var i = 0; i < totalPages; i++) {
+                var errorsOnPage = validationErrors.filter(error => error.page === i);
+                if(errorsOnPage) {
+                    errors.push(`Question${errorsOnPage.length > 1 ? 's' : ''} ${errorsOnPage.map(error => error.question + 1)} on page ${i+1} ${errorsOnPage.length > 1 ? 'are' : 'is'} not answered`);
+                }
+            }
             this.errors = errors;
         },
         sendJsonData() {
